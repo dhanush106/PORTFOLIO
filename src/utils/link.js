cf1563fde4f4bbbbff8415f8160942957ed48355
dhanush106 <sarpoordhanush@gmail.com>
2026-03-07 23:55:40 +0530

    feat:added utils page with all the links

---
+++ src/utils/link.js
@@ -0,0 +1,7 @@
+const links = {
+    "github": "https://github.com/dhanush106",
+    "linkedin": "https://www.linkedin.com/in/dhanush-s-06b35a257/",
+    "leetcode": "https://leetcode.com/u/dhanush106/",
+    "hackerrank": "https://www.hackerrank.com/profile/dhanush106",
+    "email": "[EMAIL_ADDRESS]",
+}
